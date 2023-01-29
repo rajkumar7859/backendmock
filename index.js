d97d0007326d79a70a6fdb9dd3f0ff41cb45fe98
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-01-29 14:22:45 +0530

    de

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { default: mongoose } = require('mongoose');
-const Usermodle = require('./model/user.model');
+const Usermodle = require('./src/model/user.model');
 const cors=require("cors")
 require("dotenv").config()
 
